d6b80469e38e99c0e5663bbc96a66659b23c9404
kevin-ricou <52195619+kevin-dev-31@users.noreply.github.com>
2019-11-18 10:34:31 +0100

    white night

--- pages/contact.php
+++ pages/contact.php
@@ -1,66 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>A propos de moi</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="../style.css" />	
-</head>
-<body id="about">
-	<div>
-		<a href="../index.php">LOGO</a>
-	</div>	
-	<div class="cont">
-		<h1>Me contacter</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore.</p>		
-	</div>
-        <h2>Inscription</h2>
-
-        {% if erreur %}
-            <div class="alert alert-danger col-md-8">{{ erreur|join('<br>')|raw }}</div>
-        {% endif %}
-
-        {% if msg %}
-            <div class="alert alert-success col-md-8">{{ msg }}</div>
-        {% endif %}
-
-        <form method="post" class="col-md-8">
-
-            <div class="row">
-                <div class="col">
-                    <input type="text" name="name" class="form-control" placeholder="Nom">
-                </div>
-                <div class="col">
-                    <input type="text" name="firstname" class="form-control" placeholder="Prénom">
-                </div>
-            </div>
-
-            <input type="text" name="entreprise" class="form-control" placeholder="Entreprise">
-
-            <input type="text" name="message" class="form-control" placeholder="Votre message">
-
-
-            <input type="phone" name="phone" class="form-control" placeholder="Télephone">
-
-            <input type="email" name="email" class="form-control" placeholder="Email">
-
-
-
-            <button class="btn-lg btn-form" type="submit">
-                Envoyer
-            </button>
-
-        </form>
-
-
-
-
-
-
 <?php
+require '../vendor/autoload.php';
 /* import de respect validation*/
 use Respect\Validation\Validator as v;
 
@@ -71,24 +10,17 @@ use PHPMailer\PHPMailer\Exception;
 
 
         if (!empty($_POST)) {
+
             $safe = array_map('trim', array_map('strip_tags', $_POST));
-            $em = $this->getDoctrine()->getManager();
-            $usersFound = $em->getRepository(Users::class)->findBy(['email' => $safe['email']]);
 
             $errors = [
                 (!v::length(2, 50)->validate($safe['name'])) ? 'Le nom doit comporter entre 2 et 50 caractères ' : null,
                 (v::numeric()->validate($safe['name'])) ? 'Le nom ne peut être numérique ' : null,
                 (!v::length(2, 50)->validate($safe['firstname'])) ? 'Le prénom doit comporter entre 2 et 50 caractères ' : null,
                 (v::numeric()->validate($safe['firstname'])) ? 'Le prénom ne peut être numérique ' : null,
-                (!v::length(5, 120)->validate($safe['address'])) ? 'L\'adresse est invalide ' : null,
-                (!v::postalCode('FR')->validate($safe['pC'])) ? 'Code postal invalide ' : null,
-                (!v::length(2, 120)->validate($safe['city'])) ? 'La ville n\'est pas valide ' : null,
-                (v::numeric()->validate($safe['city'])) ? 'La ville n\'est pas valide ' : null,
                 (!v::phone()->validate($safe['phone'])) ? 'Le numéro de téléphone n\'est pas valide ' : null,
                 (!v::email()->validate($safe['email'])) ? 'L\'email n\'est pas valide ' : null,
-                (v::notEmpty()->validate($usersFound)) ? 'Cet email existe déjà' : null,
-                (!v::notEmpty()->length(8, null)->regex('/[a-z]+/')->regex('/[A-Z]+/')->regex('/\d+/')->validate($safe['pwd'])) ? 'Le mot de passe doit comporter 1 maj, 1 chiffre, 8 caractères min' : null,
-                (!v::equals($safe['pwd'])->validate($safe['pwdVerif'])) ? 'Les mots de passe ne sont pas identiques' : null,
+                (!v::length(10, 1500)->validate($safe['message'])) ? 'Le message doit comporter entre 10 et 1500 caractères ' : null,
             ];
 
             $errors = array_filter($errors);
@@ -116,27 +48,101 @@ use PHPMailer\PHPMailer\Exception;
                 $mail->SMTPAuth = true; //serveur sécurisé
                 $mail->Port = 465; //port utilisé par le serveur
                 $mail->SMTPSecure = 'ssl'; //certificat SSL
-                $mail->Username = 'pratlong.estelle.test@gmail.com'; //login
-                $mail->Password = 'jerestelle@18'; //mot de passe
-                $mail->AddAddress($safe['email']); //destinataire
-                $mail->SetFrom('pratlong.estelle.test@gmail.com'); //expediteur
-                $mail->Subject = 'Inscription South-West-Store'; //sujet
+                $mail->Username = 'ricoukevin07@gmail.com'; //login
+                $mail->Password = 'citroenc4'; //mot de passe
+                $mail->AddAddress('ricoukevin@yahoo.fr'); //destinataire
+                $mail->SetFrom($safe['email']); //expediteur
+                $mail->Subject = 'Contact site CV'; //sujet
                 // le corps du mail au format HTML
-                $mail->Body = '<h1>Bienvenue ' . $safe['firstname'] . '</h1>
-                    <p>Toute l\'equipe vous souhaite la bienvenue chez South-West-Store</p>
-                    <p>Vous pouvez dès maintenant vous connecter sur votre espace personnel.</p>
-                    <p>A tres vite sur South-West-Store!</p>
-                    </br>
-                    <p>Ne pas repondre</p>';
+                $mail->Body = '<html>
+
+                        <head>
+                            <style>
+                                h1 {
+                                    color: green;
+                                }
+                            </style>
+                        </head>
+
+                        <body>
+                            <h1>Message de ' . $safe['email'] . '</h1>
+                            <p>Nom: ' . $safe['name'] . '</p>
+                            <p>Objet: ' . $safe['company'] . '</p>
+                            <p>Message: ' . $safe['message'] . '</p>
+                        </body>
+
+                        </html>';
 
                 // envoi du message
                 if ($mail->Send()) {
-                    $msg = 'Votre inscription est validé, bienvenue chez South-West-Store. Un mail de confirmation vient de vous être envoyé ';
-                }
+                   echo 'Votre mail à été envoyé. Merci de nous avoir contacté';
+                } else echo '<p>Une erreur est survenue, veuillez réessayer plus tard ' . $mail->ErrorInfo . '</p>';
+                
             } // count($errors)         
         } // if !empty($_POST)
+    ?>    
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A propos de moi</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="../style.css" />	
+</head>
+<body id="about">
+	<div>
+		<a href="../index.php">LOGO</a>
+	</div>	
+	<div class="container">
+		<h1>Me contacter</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore.</p>	
+
+    <?php if (isset($msg)) : ?>
+        <p style="color:green;">ok</p>
+
+    <?php elseif (count($errors) > 0) : // Si j'ai des erreurs, je les affiche 
+        ?>
+        <p style="color:red"><?= implode('<br>', $errors); ?></p>
+    <?php endif; ?>
+
+        <form method="post" class="col-md-8">
+
+            <div class="row">
+                <div class="col">
+                    <input type="text" name="name" class="form-control" placeholder="Nom" required>
+                </div>
+                <div class="col">
+                    <input type="text" name="firstname" class="form-control" placeholder="Prénom" required>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <input type="phone" name="phone" class="form-control" placeholder="Télephone" required>
+                </div>
+                <div class="col">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required>
+                </div>
+            </div>
+
+            <input type="text" name="company" class="form-control" placeholder="Entreprise">
+
+        
+                <textarea class="form-control bg-form" name="message" rows="5" placeholder="Votre message" required></textarea>
+            
+            <button class="btn-lg btn-form" type="submit">
+                Envoyer
+            </button>
+
+        </form>
+    </div>
 
 
-	 require '../inc/footer.php'; ?>
+
 </body>
 </html>
+
