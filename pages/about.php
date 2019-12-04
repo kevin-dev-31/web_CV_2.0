2fe9a3c9b3b543eda08daefafaaf55b51a2f9078
kevin-kev-31 <ricoukevin@yahoo.fr>
2019-12-04 18:56:08 +0100

    04/12

--- pages/about.php
+++ pages/about.php
@@ -4,42 +4,54 @@
 </head>
 <body id="about">
 	<main>
-    	<div class="indexLink">
-    		<a href="../index.php"><img src="../img/logo.png" alt="logo du site, lien vers l'accueil"></a>
-    	</div>	
-		<div class="container mt-5 mb-5">
-			<h2>Qui suis-je?</h2>
-			<p>
-				Moi c’est Kévin, j’ai 31 ans, je suis passionnée par les jeux vidéo, mes parents on acheter une séga master système quand j’avais 2 ans, depuis ce jours je n’ai jamais arrêté 😁 je suis mordu de rétro gaming, ce qui fait que je collectionne les consoles et les jeux (surtout séga et nintendo).
-			</p>
-			<p>
-				Ah, je suis aussi fada de sport de glisse aussi, je fais du surf du snow et du bmx, ca permet aussi de coupée avec les écrans de temps en temps ^^
-			</p>
-		</div>
-		<div>
-			<img src="../img/test.png" class="img-fluid" alt="photo de sport me représentant">
-		</div>
+		<div class="container">	
+	    	<div class="indexLink">
+	    		<a href="../index.php"><img src="../img/logo.png" alt="logo du site, lien vers l'accueil"></a>
+	    	</div>
 
-			
-		<div class="container mt-5 mb-5">
-			<h2>Mon parcours</h2>
-			<p>
-				Issu d'une formation de carrossier, j'ai travaillé de nombreuses années dans plusieurs entreprises en intérim. J'ai occupé différentes fonctions, telles que technicien SAV aéronautique, responsable de secteur et employé polyvalent. 
-			</p>
-			<p>
-				Passionné depuis toujours par l'informatique et les jeux vidéo, j'ai décidé qu'il était temps de me diriger vers un emploi dans lequel je pourrais m’épanouir. 
-			</p>
-			<p>
-				Tout d’abord autodidacte, j'ai intégré la formation de développeur web et web mobile chez WebForce3 Toulouse. A l’issue de ma formation j’ai eu la chance de pouvoir faire un stage de cinq semaines au sein de Verrywell digital, anciennement Lounce. Ce fut pour moi une très bonne expérience avec des activités très varier, ce qui ma aussi permis de découvrir le fonctionnement d’une agence web, c’était génial !
-			</p>
-			<p>
-				Mon objectif pour l'avenir, intégrer une équipe dynamique, m’épanouir dans mon travail et apprendre chaque jour de nouvelles choses, ainsi que de continuer à jouer à Pokémon, sans aucune honte, mais ça, c'est une autre histoire!
-			</p>
-		</div>
-
-		<div>
-			<img src="../img/test.png" class="img-fluid" alt="photo de sport me représentant">
-		</div>
+	    	<div class="first">
+				<div class="row">
+					<div class="col-md-8 offset-md-4">
+						<h2>Qui suis-je?</h2>
+						<img src="../img/about/stack.jpg" class="img-fluid" alt="">
+						<p>Moi c’est Kévin, j’ai 31 ans, je suis passionnée par les jeux vidéo, mes parents on acheter une séga master système quand j’avais 2 ans, depuis ce jours je n’ai jamais arrêté 😁 je suis mordu de rétro gaming, ce qui fait que je collectionne les consoles et les jeux (surtout séga et nintendo).
+						<br>
+						Ah, je suis aussi fada de sport de glisse aussi, je fais du surf du snow et du bmx, ca permet aussi de coupée avec les écrans de temps en temps ^^
+						</p>
+					</div>
+				</div>    		
+	    	</div>
+		
+	    	<div class="row second">
+	    		<div class="col-md-6">
+	    			<img src="../img/about/stack.jpg" class="img-fluid" alt="">
+	    		</div>
+	    		<div class="col-md-6">
+					<h2>Mon parcours</h2>
+					<p>
+						Issu d'une formation de carrossier, j'ai travaillé de nombreuses années dans plusieurs entreprises en intérim. J'ai occupé différentes fonctions, telles que technicien SAV aéronautique, responsable de secteur et employé polyvalent. 
+					</p>
+					<p>
+						Passionné depuis toujours par l'informatique et les jeux vidéo, j'ai décidé qu'il était temps de me diriger vers un emploi dans lequel je pourrais m’épanouir. 
+					</p>
+				</div>
+	    	</div>
+    	</div>
+    	<div class="back">
+    		<div class="container">
+    			<div>
+					<h2>formation</h2>
+					<p>
+						Tout d’abord autodidacte, j'ai intégré la formation de développeur web et web mobile chez WebForce3 Toulouse. A l’issue de ma formation j’ai eu la chance de pouvoir faire un stage de cinq semaines au sein de Verrywell digital, anciennement Lounce. Ce fut pour moi une très bonne expérience avec des activités très varier, ce qui ma aussi permis de découvrir le fonctionnement d’une agence web, c’était génial !
+					</p>
+					<hr>
+					<h2>objectif</h2>
+					<p>
+						Mon objectif pour l'avenir, intégrer une équipe dynamique, m’épanouir dans mon travail et apprendre chaque jour de nouvelles choses, ainsi que de continuer à jouer à Pokémon, sans aucune honte, mais ça, c'est une autre histoire!
+					</p>
+				</div>
+    		</div>
+    	</div>
 
 
 	</main>
